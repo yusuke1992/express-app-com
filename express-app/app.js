85a2e4b6a1df7197427f9c7e3d864641aa692224
yusuke1992 <adsoofmelk1327@gmail.com>
2019-05-16 15:59:42 +0900

    socket関連の記述

--- express-app/app.js
+++ express-app/app.js
@@ -1,20 +1,44 @@
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 7000;
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var after = require('./routes/after');
 var anotherAfter = require('./routes/another_after');
 var login = require('./routes/login');
+var chat = require('./routes/chat');
+
+
 var setUser = require('./setUser');
 var setCookie = require('./setCookie');
 
+app.get('/chat' , function(req, res){
+  res.sendFile( __dirname + "/views/" + "chat.html" );
+});
 
-var app = express();
+io.on('connection',function(socket){
+  socket.on('message',function(msg){
+      console.log('message: ' + msg);
+      io.emit('message', msg);
+  });
+});
+
+http.listen(PORT, function(){
+ console.log('server listening. Port:' + PORT);
+});
+
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -42,6 +66,7 @@ app.use('/users', usersRouter);
 app.use('/login', login);
 app.use('/after', setUser, after);
 app.use('/another_after', setUser, anotherAfter);
+app.use('/chat', chat);
 
 
 // catch 404 and forward to error handler
